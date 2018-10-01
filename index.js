999529b40e816224bb7887ffece8d4c79e9e7e1a
venigalla <venigalla83@hotmail.com>
2018-10-01 13:14:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
  * A Bot for Slack!
  */
 
-
 /**
  * Define a function for initiating a conversation on installation
  * With custom integrations, we don't have a way to find out who installed us, so we can't message them :(
