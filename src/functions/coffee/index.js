193101f8aa78fde0a6c6702116b99509f6f90d97
Nigel Alford <nigel@callrail.com>
2019-05-12 01:03:46 -0400

    added 1st GET stub for google cloud function

---
+++ src/functions/coffee/index.js
@@ -0,0 +1,15 @@
+// [START functions_helloworld_get]
+/**
+ * HTTP Cloud Function.
+ * This function is exported by index.js, and is executed when
+ * you make an HTTP request to the deployed function's endpoint.
+ *
+ * @param {Object} req Cloud Function request context.
+ *                     More info: https://expressjs.com/en/api.html#req
+ * @param {Object} res Cloud Function response context.
+ *                     More info: https://expressjs.com/en/api.html#res
+ */
+
+exports.coffeeGET = (req, res) => {
+  res.send('Ethopian coffee runs the world');
+};
