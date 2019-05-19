5fec4ce746a7f2ad0d528a41d896ee7d0add6415
Nigel Alford <nigel@callrail.com>
2019-05-19 01:04:52 -0400

    added google cloud http functions, get and post work in prod

--- src/functions/coffee/index.js
+++ src/functions/coffee/index.js
@@ -1,15 +1,72 @@
-// [START functions_helloworld_get]
-/**
- * HTTP Cloud Function.
- * This function is exported by index.js, and is executed when
- * you make an HTTP request to the deployed function's endpoint.
- *
- * @param {Object} req Cloud Function request context.
- *                     More info: https://expressjs.com/en/api.html#req
- * @param {Object} res Cloud Function response context.
- *                     More info: https://expressjs.com/en/api.html#res
- */
+require('dotenv').config();
+const Firestore = require('@google-cloud/firestore');
 
-exports.coffeeGET = (req, res) => {
-  res.send('Ethopian coffee runs the world');
+// TODO: environment variables
+
+const PROJECTID = FIREBASE_ID;
+const COLLECTION_NAME = FIREBASE_COLLECTION;
+const firestore = new Firestore({
+  projectId: PROJECTID,
+  timestampsInSnapshots: true
+});
+
+exports.coffeeHTTP = (req, res) => {
+  if (req.method === 'DELETE') throw 'not yet built';
+  if (req.method === 'POST') {
+    // store/insert a new document
+    const data = req.body || {};
+    const id = data.id || data.name.concat(data.roaster);
+    const increment = Firestore.FieldValue.increment(1);
+
+    if (data.likes) {
+      return firestore
+        .collection(COLLECTION_NAME)
+        .doc(id)
+        .update({ likes: increment })
+        .then(doc => res.status(200).send(doc))
+        .catch(err => res.status(404).send({ error: 'unable to store', err }));
+    }
+    if (data.dislikes) {
+      return firestore
+        .collection(COLLECTION_NAME)
+        .doc(id)
+        .update({ dislikes: increment })
+        .then(doc => res.status(200).send(doc))
+        .catch(err => res.status(404).send({ error: 'unable to store', err }));
+    }
+
+    return firestore
+      .collection(COLLECTION_NAME)
+      .doc(data.name.concat(data.roaster).replace(/ /g, ''))
+      .set({
+        // id: data.name.concat(data.roaster),
+        name: data.name,
+        roaster: data.roaster,
+        brew: null,
+        likes: 0,
+        dislikes: 0
+      })
+      .then(doc => res.status(200).send(doc))
+      .catch(err => res.status(404).send({ error: 'unable to store', err }));
+  }
+  if (req.method === 'GET') {
+    // read/retrieve an existing document by id
+    if (!(req.query && req.query.id)) {
+      return res.status(404).send({ error: 'No Id' });
+    }
+    const id = req.query.id.replace(/[^a-zA-Z0-9]/g, '').trim();
+    if (!(id && id.length)) {
+      return res.status(404).send({ error: 'Empty Id' });
+    }
+    return firestore
+      .collection(COLLECTION_NAME)
+      .doc(id)
+      .then(doc => {
+        if (!(doc && doc.exists)) {
+          return res.status(404).send({ error: 'Unable to find the document' });
+        }
+        return res.status(200).send(doc.data());
+      })
+      .catch(err => res.status(404).send({ error: err }));
+  }
 };
